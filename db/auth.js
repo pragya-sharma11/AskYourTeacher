3d506df8850ecc836ff6b6bff559f0290de507d1
github <pragya.sharma2_cs19@gla.ac.in>
2021-09-18 23:20:59 +0530

    commit

--- db/auth.js
+++ db/auth.js
@@ -19,9 +19,6 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  module.exports={
-    app
-  }
 
   /**
    * npm install firebase
