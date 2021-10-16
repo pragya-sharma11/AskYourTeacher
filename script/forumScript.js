0444caeef3397250ad817e4df0f68fe93b442379
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-16 11:21:55 +0530

    commit

--- script/forumScript.js
+++ script/forumScript.js
@@ -2,4 +2,13 @@ window.onload=()=>{
     var ul = document.getElementById('list')
     var input = document.getElementById('input')
     var submit = document.getElementById('submit')
+    var count = 0
+    submit.onclick=()=>{
+        let val = input.value
+        count++;
+        console.log(val)
+        let li = document.createElement('li')
+        li.innerText = `Question ${count} : ` + val
+        ul.appendChild(li)
+    }
 }
